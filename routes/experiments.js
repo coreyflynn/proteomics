fda51fac7619424df24f65707c71a5b87bc42512
root <root@ip-172-31-47-184.us-west-2.compute.internal>
2014-12-08 14:29:50 -0500

    test

--- routes/experiments.js
+++ routes/experiments.js
@@ -8,44 +8,113 @@ var proteingroups = require('../models/proteingroups');
 var modspecpeptides = require('../models/modspecpeptides');
 var db = require('../models/database');
 
+
+Array.prototype.unique = function() {
+    var a = this.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+};
+
 /* Blank page (help) */
 router.get('/', function(req, res) {
-	console.log(db.collections);
-	var array = [];
-	for (var i in db.collections){array.push(db.collections[i]);}
-	res.render('search', {collections: array});
-});
+	var query = req.query.q;
+	var retCols = req.query.r;
+	var distinct = req.query.d;
 
-/* GET query page. */
-router.get('/proteingroups', function(req, res) {
+	if (query    == null) query    = {'gene names':'ACTB'};
+	if (retCols  == null) retCols  = {};
 
-        var toFind = {};
-        if (req.query.q != null) toFind = JSON.parse(req.query.q);
-        proteingroups.find(toFind, function(err, results) {
-                res.json(results);
-        });
-});
+	if (distinct != null) results = [];
+	else results = {};
+	
+	async.series([
+		function (callback) {async.parallel([
+			function (callback) {
+				console.log("Starting e call");
+				if (distinct == null) {
+					evidence.find(query, retCols, function (error, queryResults) {
+						results.evidence = queryResults; callback ();
+					})
+				}
+				else {
+					evidence.distinct(distinct, function (error, queryResults) {
+						results = results.concat(queryResults); callback();
+					})
+				}
+                                console.log("Ending e call");
+			},
+			function (callback) {
+                                console.log("Starting m call");
+	                        if (distinct == null) {
+	                                modspecpeptides.find(query, retCols, function (error, queryResults) {
+	                                        results.modSpecPeptides = queryResults; callback();
+	                                })
+	                        }
+	                        else {
+	                                modspecpeptides.distinct(distinct, function (error, queryResults) {
+	                                        results = results.concat(queryResults); callback();
+	                        	})
+				}
+                                console.log("Ending m call");
+	                },
+			function (callback) {
+                                console.log("Starting p call");
+	                        if (distinct == null) {
+	                                peptides.find(query, retCols, function (error, queryResults) {
+	                                        results.peptides = queryResults; callback();
+	                                })
+	                        }
+	                        else {
+	                                peptides.distinct(distinct, function (error, queryResults) {
+	                                        results = results.concat(queryResults); callback();
+	                        	})
+				}
+                                console.log("Ending p call");
+	                },
+			function (callback) {
+                                console.log("Starting g call");
+	                        if (distinct == null) {
+	                                proteingroups.find(query, retCols, function (error, queryResults) {
+	                                        results.proteinGroups = queryResults; callback();
+	                                })
+	                        }
+	                        else {
+	                                proteingroups.distinct(distinct, function (error, queryResults) {
+	                                        results = results.concat(queryResults); callback();
+	                        	})
+				}
+                                console.log("Ending g call");
+	                }
+		],
+	
+		function (error) {console.log("Calling back");callback();}
+	
+		)},
+		
+		// Sort the array (part two)
+		function (callback) {
+			if (distinct != null) {
+				console.log("Uniquifying array...");
+				results = results.unique();
+			}
+			callback();
+		}
+		],
+		function (error) {
+			res.json(results);
+		});
 
-router.get('/experiments', function(req, res) {
-
-	var toFind = {};
-	if (req.query.q != null) toFind = JSON.parse(req.query.q);
-	experiments.find(toFind, function(err, results) {
-		res.json(results);
-	});
-});
-
-router.get('/evidence', function(req, res) {
-
-        var toFind = {};
-        if (req.query.q != null) toFind = JSON.parse(req.query.q);
-        evidence.find(toFind, function(err, results) {
-                res.json(results);
-        });
 });
 
 router.get('/bygene', function(req, res) {
-        var geneName = req.query.g; var display = req.query.d;
+        var geneName = req.query.g; var display = req.query.display;
+	var distinct = req.query.d;
         var criteria = {'gene names':geneName};
         var retFields = {expID:1, 'gene names':1, _id:0};
         var retFieldsDistinct = {expID:1, id:1, _id:0};
@@ -131,9 +200,7 @@ router.get('/bygene', function(req, res) {
                                         // Check to see if it's unique
 					var counter; var add = true;
 					for (counter = 0; counter < results.distinct.experiments.length; counter++) {
-						console.log("Comparing " + fullDistinctInfo[0] + " and " + results.distinct.experiments[counter]);
 						if (JSON.stringify(fullDistinctInfo[0]) == JSON.stringify(results.distinct.experiments[counter])) {
-							console.log("They look so goddamn like the same person");
 							add = false;
 							break;
 						}
@@ -157,7 +224,6 @@ router.get('/bygene', function(req, res) {
                         throw(err);
                 }
                 if (display == "table") {
-                        console.log("Experiments array: " + JSON.stringify(results.distinct.experiments));
                         res.render('resultsTable', {results:JSON.stringify(results.distinct.experiments)});
                 }
                 else {
