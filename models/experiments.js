16c63841c30c7c15460480ded22ef59cbe67cdc0
rfederici <federici_roman@bah.com>
2014-12-15 14:20:16 -0500

    Attempt at reorganization

--- models/experiments.js
+++ models/experiments.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var database = require('./database.js');
+
 //mongoose.connect('mongodb://localhost:27017/proteomics');
 
 var Experiment = mongoose.model('Experiment', {path: String}, 'experiments');
--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var async = require('async');
-var evidence = require('../models/evidence');
-var experiments = require('../models/experiments');
-var peptides = require('../models/peptides');
-var proteingroups = require('../models/proteingroups');
+var express         = require('express');
+var router          = express.Router();
+var async           = require('async');
+
+var experiments     = require('./experiments');
+
+var evidence        = require('../models/evidence');
+var peptides        = require('../models/peptides');
+var proteingroups   = require('../models/proteingroups');
 var modspecpeptides = require('../models/modspecpeptides');
-var db = require('../models/database');
 
 
 Array.prototype.unique = function() {
@@ -21,72 +22,157 @@ Array.prototype.unique = function() {
     return a;
 };
 
-router.get('/', function(req, res) {
-        var query = req.query.q;
-        var retCols = req.query.r;
-        var distinct = req.query.d;
+router.get('/experiments', experiments)
 
-        if (query    == null) query    = {'gene names':'ACTB'};
-        if (retCols  == null) retCols  = {'gene names':1, 'id':1};
+router.get('/', function(req, res) {
+        var query     = {};
+        var retCols   = {};
+        var colsToAdd = [];
+        var related   = req.query.r;
+
+        try {query     = JSON.parse(req.query.q);}
+                catch (e) {res.json({error:"Problem parsing query parameter", exception: e.toString()});return;}
+
+    if (req.query.f != null) {
+            try {retCols   = JSON.parse(decodeURIComponent(req.query.f));}
+                catch (e) {res.jsonp({error:"Problem parsing return values"});return;}
+    }
+
+        if (req.query.col == null)
+            colsToAdd = ["evidence","modificationSpecificPeptides","peptides","proteinGroups"];
+        else
+            try {colsToAdd = JSON.parse(req.query.col);}
+            catch (e) {res.jsonp({error:"Problem parsing collections"});return;}
 
         if (distinct != null) {
-        	console.log("Distinct isn't null");
-        	results = [];
+                results = [];
         }
         else results = {};
 
-
+    /////////////////////////////////////////////////////
         /* Two steps to be run in series:
         *    1. Make the calls to the database (in parallel)
         *    2. Make array unique
         *    Callback: Print JSON results */
+    /////////////////////////////////////////////////////
 
         async.series([
 
-        	// Step 1 - Make DB calls in parallel
-        	function (outerCB) {
-            	async.parallel([
-                        function (callback) {
-                                if (distinct == null) {
-                                        evidence.find(query, retCols, function (error, queryResults) {
-                                                results.evidence = queryResults;
-                                                callback();
-                                        });
-                                }
-                                else {
-                                        evidence.distinct(distinct, function (error, queryResults) {
-                                                results = results.concat(queryResults);
-                                                callback();
-                                        });
-                                }
-                        },
+            // Step 1 - Make DB calls in parallel
+        /////////////////////////////////////
+            function (outerCB) {
+                async.parallel([
+
+        // EVIDENCE //
+                    function (callback) {
+                        if (colsToAdd.indexOf('evidence') > -1) {
+                            if (distinct == null) {
+                                    evidence.find(query, retCols, function (error, queryResults) {
+                                            results.evidence = queryResults;callback();
+                                    });
+                            }
+                            else {
+                                    evidence.distinct(distinct, query, function (error, queryResults) {
+                            results = results.concat(queryResults);callback();
+                                    });
+                            }
+                        }
+                        else {callback();}
+                    },
+
+        // MODIFICATION SPECIFIC PEPTIDES //
+                    function (callback) {
+                        if (colsToAdd.indexOf('modificationSpecificPeptides') > -1) {
+                            if (distinct == null) {
+                                    modspecpeptides.find(query, retCols, function (error, queryResults) {
+                                            results.modSpecPeptides = queryResults;callback();
+                                    });
+                            }
+                            else {
+                                    modspecpeptides.distinct(distinct, query, function (error, queryResults) {
+                                        results = results.concat(queryResults);callback();
+                    });
+                            }
+                        }
+                        else {callback();}
+                    },
+
+        // PEPTIDES //
+                    function (callback) {
+                        if (colsToAdd.indexOf('peptides') > -1) {
+                            if (distinct == null) {
+                                    peptides.find(query, retCols, function (error, queryResults) {
+                        results.peptides = queryResults;callback();
+                                    });
+                        }
+                            else {
+                                    peptides.distinct(distinct, query, function (error, queryResults) {
+                                            results = results.concat(queryResults);callback();
+                                    });
+                            }
+                        }
+                        else {callback();}
+                    },
+
+        // PROTEIN GROUPS //
+                    function (callback) {
+                        if (colsToAdd.indexOf('proteinGroups') > -1) {
+                            if (distinct == null) {
+                                    proteingroups.find(query, retCols, function (error, queryResults) {
+                                            results.proteinGroups = queryResults;callback();
+                                    });
+                            }
+                            else {
+                                    proteingroups.distinct(distinct, query, function (error, queryResults) {
+                                            results = results.concat(queryResults);callback();
+                                    });
+                            }
+                        }
+                        else {callback();}
+                    }
                 ],
 
-                // All DB calls made - proceed to step 2
+        // All DB calls made - proceed to step 2
                 function (error) {
-                	if(error)
-                		console.log(error);
-                	outerCB();
+                    if(error)
+                        console.log(error);
+                    outerCB();
                 })
 
             },
 
-            // Step 2 - Make array unique
+        // Step 2 - Make array unique
+        /////////////////////////////
             function (outerCB) {
-                    if (distinct != null) {
-                            console.log("Uniquifying array...");
-                            results = results.unique();
-                    }
-                    outerCB();
+                if (distinct != null) {
+
+                    // Make array unique, then alphabetize.
+                    results = results.unique();
+                    results.sort(function(a, b) {
+                        if (a.toLowerCase() < b.toLowerCase()) return -1;
+                        if (a.toLowerCase() > b.toLowerCase()) return 1;
+                        return 0;
+                    });
+
+                    var space = results.indexOf("");
+                    if (space > -1) results.splice(space, 1);
+                }
+                outerCB();
             }
 
-		],
+        ],
 
-		// Callback - print JSON results
+        // Callback - print JSON results
+        ////////////////////////////////
         function (error) {
-        	if(error)
-        		console.log(error);
+                if(error)
+                        console.log(error);
             res.jsonp(results);
         });
 
 });
+
+
+
+
+module.exports = router;
