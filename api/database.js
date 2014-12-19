43588338e60e90614c4cdebde56a9478564cdad4
Corey Flynn <flynn.corey@gmail.com>
2014-12-19 11:48:32 -0500

    configured for local dev

--- api/database.js
+++ api/database.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/proteomics');
+mongoose.connect('mongodb://ec2-54-68-96-157.us-west-2.compute.amazonaws.com/:27017/proteomics');
 
 
 mongoose.connection.on('open', function (ref) {
     mongoose.connection.db.collectionNames(function (err, names) {
 	var collections = [];
-	
+
 	for (var i = 0; i < names.length; i++) {
 	  collections.push(names[i].name);
 	}
