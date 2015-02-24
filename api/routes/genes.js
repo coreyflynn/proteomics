15a5ba5aea1df1b7653c325c4a9fa27755056447
rfederici <federici_roman@bah.com>
2015-02-24 11:57:42 -0500

    fine-tune

--- api/routes/genes.js
+++ api/routes/genes.js
@@ -2,23 +2,21 @@ var express = require('express');
 var router  = express.Router();
 var async   = require('async');
 
-var geneNames        = require('../models/geneNames');
+var geneNames = require('../models/geneNames');
 
 router.get('/', function(req, res) {
 
 	var query = req.query.q;
 	var limit = req.query.l;
 
-	if(limit == null)
-		limit = 20;
+	if (query == null) query = {}
+	if (limit == null) limit = 20;
 
-	if(query == null){
-		geneNames.find().limit(limit).exec(function (error, queryResults) {
-			if (error)
-				console.log(error);
-			else
-				res.jsonp(queryResults);
-		})
-	}
+	geneNames.find(query,{"gene":1,"_id":0}).limit(limit).exec(function (error, queryResults) {
+		if (error)
+			console.log(error);
+		else
+			res.jsonp(queryResults);
+	})
 
 }
