4264c3c6700c8efd0025dfc8667df6d563811267
rfederici <federici_roman@bah.com>
2015-02-24 12:19:30 -0500

    Cleaned up the logic for adding a query

--- api/routes/genes.js
+++ api/routes/genes.js
@@ -6,10 +6,14 @@ var geneNames = require('../models/geneNames');
 
 router.get('/', function(req, res) {
 
-	var query = req.query.q;
+	var query = {};
 	var limit = req.query.l;
 
-	if (query == null) query = {}
+	if (req.query.q != null) {
+		try { query = JSON.parse(req.query.q) }
+		catch (e) { res.json({error:"Problem parsing query parameter", exception: e.toString()}); return; }
+	}
+	
 	if (limit == null) limit = 20;
 
 	geneNames.find(query,{"gene":1,"_id":0}).limit(limit).exec(function (error, queryResults) {
