cd1c209acb38db9b642e42e6a465b71a2fb3906c
rfederici <federici_roman@bah.com>
2015-02-25 12:02:55 -0500

    Experiment finder works as expected.

--- api/routes/experiments.js
+++ api/routes/experiments.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router  = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var experiments     = require('../models/experiments');
 
 router.get('/', function(req, res) {
-    
+
     var experiment = {};
 
-    if (req.query.exp != null)
-        experiment = {_id:ObjectId(exp)};
+    if (req.query.exp != null){
+        obj = new ObjectId(req.query.exp);
+        experiment = {_id:obj};
+    }
     else
         res.jsonp({error:"No experiment given."});
 
