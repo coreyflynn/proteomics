a52378826dc9709beb0574369e914a7bddfab5c2
rfederici <federici_roman@bah.com>
2015-03-04 13:18:46 -0500

    filling out mod script

--- modifications.py
+++ modifications.py
@@ -7,68 +7,80 @@ from pymongo import MongoClient
 from pymongo.collection import Collection
 from bson.objectid import ObjectId
 
+#  Example data structure for geneNames
+#
+#  {
+#    gene: "ACTG",
+#    modifiedSequences: {
+#      _AGFAGDDAPR_: {
+#        54be605e7ab3f3896a57df79:{
+#          intensity: [120320000, 324640000],
+#          modifications: [mod1, mod2]
+#      }
+#    }
+#  }
+#
+
+#  Example data structure for modifications
+#
+#  {
+#    modifiedSequence: "_AGFAGDDAPR_"
+#    experiments: {
+#      54be605e7ab3f3896a57df79: {
+#        intensity: [120320000, 324640000],
+#        modifications: [mod1, mod2]
+#      }
+#    }
+#  }
+#
+
+
 # Connects to the Mongo client
 client = MongoClient('localhost', 27017)
 evidence = client.proteomics.evidence
 modSeqs = client.proteomics.modifiedSequences
 
 # For each gene, use this process to see if it exists.
-def process (expid, modifiedSequence, intensity):
+def process (expid, modifiedSequence, intensity, mods):
 
-    res = geneNames.find({"gene":geneName})
+    criteria = {"modifiedSequence":modifiedSequence}
+    res = modSeqs.find(criteria)
 
     # Does gene exist?
 
     if res.count() == 0:
-    # No - create new document for gene
-        print("New gene found: " + geneName)
+    # No - create new document for mod
+        print("New mod found: " + modifiedSequence)
 
-        obj = {"gene":geneName, "modifications":{}}
+        obj = {"modifiedSequence":modifiedSequence, "experiments":{}}
         if intensity != "":
-            obj["modifications"][modifiedSequence] = {str(expid):[intensity]}
+            obj["experiments"][expid] = {intensity:[intensity], modifications: mods}
         else:
-            obj["modifications"][modifiedSequence] = {str(expid):[]}
-        geneNames.insert(obj)
+            obj["experiments"][expid] = {str(expid):[], modifications: mods}
+        modSeqs.insert(obj)
 
     else:
-    # Yes - append to gene document
+    # Yes - append to mod document
         obj = res[0]
+        key = "experiments." + str(expid)
 
-        # Does the modification exist?
-        if modifiedSequence in obj["modifications"]:
-        # if hasattr(obj["modifications"], modifiedSequence):
+        if intensity != "":
+            toPush = {key:{intensity:[intensity], modifications: mods}
+        else:
+            toPush = {key:{intensity:[], modifications: mods}
+
+        # Does the experiment exist?
+        if str(expid) in obj["experiments"]:
         # Yes
-
-            # Does the experiment exist?
-            if str(expid) in obj["modifications"][modifiedSequence]:
-            # Yes
-                key = "modifications." + modifiedSequence + "." + str(expid)
-                toPush = {key:intensity}
-                if intensity != "":
-                    geneNames.update({"gene":geneName}, {'$push':toPush})
-
-            else:
-            # No
-                key = "modifications." + modifiedSequence + "." + str(expid)
-                if intensity != "":
-                    toPush = {key:[intensity]}
-                else:
-                    toPush = {key:[]}
-                geneNames.update({"gene":geneName}, {'$set':toPush})
+            modSeqs.update(criteria, {'$push':toPush})
 
         else:
         # No
-            key = "modifications." + modifiedSequence
-            if intensity != "":
-                toPush = {key:{str(expid):[intensity]}}
-            else:
-                toPush = {key:{str(expid):[]}}
-            geneNames.update({"gene":geneName}, {'$set':toPush})
+            modSeqs.update(criteria, {'$set':toPush})
 
-
-geneNames.drop()
+modSeqs.drop()
 
 # Loop through all gene names in the experiment collection.
 
 for document in evidence.find({},{"expID":1,"modified sequence":1,"intensity":1}).batch_size(30):
-        process(document["expID"],document["modified sequence"].upper(),document["intensity"])
+        process(document["expID"],document["modified sequence"].upper(),document["intensity"], document["modifications"])
